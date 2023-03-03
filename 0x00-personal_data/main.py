ba4b2e723cca930519f1624396a6765da114a9a1
Reid-T-W <rediettadesse100@gmail.com>
2023-03-03 04:08:57 +0300

    main file added

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -2,7 +2,15 @@
 """
 Main file
 """
-# Task 1 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Task 2 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import logging
+
+get_logger = __import__('filtered_logger').get_logger
+PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+
+print(get_logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+# Task 0 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # filter_datum = __import__('filtered_logger').filter_datum
 
 # fields = ["password", "date_of_birth"]
@@ -11,13 +19,23 @@ Main file
 # for message in messages:
 #     print(filter_datum(fields, 'xxx', message, ';'))
 
-# Task 2 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-import logging
-import re
+# Task 1 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# import logging
+# import re
 
-RedactingFormatter = __import__('filtered_logger').RedactingFormatter
+# RedactingFormatter = __import__('filtered_logger').RedactingFormatter
 
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
-formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-print(formatter.format(log_record))
+# message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+# log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+# formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+# print(formatter.format(log_record))
+
+
+# # Task 2 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# import logging
+
+# get_logger = __import__('filtered_logger').get_logger
+# PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+
+# print(get_logger.__annotations__.get('return'))
+# print("PII_FIELDS: {}".format(len(PII_FIELDS)))
