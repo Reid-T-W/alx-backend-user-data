32d8ec49038d3fda7c914420de590d9bc4d4c7cc
Reid-T-W <rediettadesse100@gmail.com>
2023-03-05 19:04:07 +0300

    type annotation added

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,6 +3,6 @@
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """ hash a password using a randomly generated salt """
     return(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
