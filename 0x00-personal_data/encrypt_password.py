65fdf3ba99f83c07a629dc977d1b49c59271e5c4
Reid-T-W <rediettadesse100@gmail.com>
2023-03-05 19:12:32 +0300

    check if a given password is correct

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ hash a password using a randomly generated salt """
     return(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ validate password """
+    return(bcrypt.checkpw(password.encode('utf-8'), hashed_password))
