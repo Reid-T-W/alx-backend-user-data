3799dec3c2b54d756bf80db9a8ff9f661c1d0c36
Reid-T-W <rediettadesse100@gmail.com>
2023-03-07 12:51:22 +0300

    decoding from base_64 complete

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import re
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,19 @@ class BasicAuth(Auth):
             return None
         # Returning the value after basic
         return (re.search(r'(?<=Basic ).*', authorization_header).group(0))
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Decodes a base64 header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_auth_header = base64.b64decode(base64_authorization_header)
+            return decoded_auth_header.decode('utf-8')
+        except base64.binascii.Error:
+            return None
