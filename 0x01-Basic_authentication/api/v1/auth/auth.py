a04500c9e3b1bf8f159e38abdaa15e9eb59a789c
Reid-T-W <rediettadesse100@gmail.com>
2023-03-07 22:06:16 +0300

    * as the end of excluded paths supported

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,24 +2,36 @@
 """ Class to manage Authentication """
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth():
     """ Class to manage Authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Checks of authuntication is required """
+        """ Checks of authuntication is required
+            Returns:
+                True if authentication is required
+                    (path is not found in excluded_paths)
+                False if authentication is not required
+                    (path is found in excluded_paths)
+        """
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        # Making the method slash tolerent
+        # # Making the method slash tolerent
         if path[-1] != '/':
             path = path + '/'
 
-        if path not in excluded_paths:
-            return True
-        else:
-            return False
+        #  Allowing * at the end of excluded paths
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '*':
+                excluded_path = excluded_path[:-1]
+            match = re.match(excluded_path, path)
+            if match is None:
+                return True
+            else:
+                return False
 
     def authorization_header(self, request=None) -> str:
         """ Handles the authorization header """
