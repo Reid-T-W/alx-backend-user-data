2148cfff121a01d265bf902927fe89910c4db6a5
Reid-T-W <rediettadesse100@gmail.com>
2023-03-07 13:48:10 +0300

    email and password extraction implemented

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Class to manage Authentication """
 from flask import request
-from typing import List, TypeVar
+from typing import List, TypeVar, Tuple
 from api.v1.auth.auth import Auth
 import re
 import base64
@@ -39,3 +39,21 @@ class BasicAuth(Auth):
             return decoded_auth_header.decode('utf-8')
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> Tuple[str, str]:
+        """
+        Extract username and password from decoded auth header
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if re.search(':', decoded_base64_authorization_header) is None:
+            return (None, None)
+        email = re.match('.*(?=:)',
+                         decoded_base64_authorization_header).group(0)
+        password = re.search('(?<=:).*',
+                             decoded_base64_authorization_header).group(0)
+        return(email, password)
