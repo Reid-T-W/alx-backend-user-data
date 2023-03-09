fa3143baa508d92df4b95d5ea78813c153f78597
Reid-T-W <rediettadesse100@gmail.com>
2023-03-09 21:22:58 +0300

    additional checks added to handle_before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -50,15 +50,19 @@ def handle_before_request() -> None:
     """Run before every request
     """
     excluded_path = ['/api/v1/status/', '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+                     '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         pass
     try:
         if auth.require_auth(request.path, excluded_path) is False:
             pass
         else:
+            # if auth.authorization_header(request) is None and \
+            #    auth.session_cookie(request) is None:
+            #     abort(401)
             request.current_user = auth.current_user(request)
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+               auth.session_cookie(request) is None:
                 abort(401)
             if request.current_user is None:
                 abort(403)
