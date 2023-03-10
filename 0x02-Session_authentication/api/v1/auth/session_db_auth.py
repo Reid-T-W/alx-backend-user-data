67249ab34be52f292393f25683090f794ac1a38a
Reid-T-W <rediettadesse100@gmail.com>
2023-03-10 05:58:03 +0300

    session saves to db

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Session saved to db """
+from api.v1.auth.session_auth import SessionExpAuth
+from api.v1.views.users import User
+import uuid
+import os
+from datetime import datetime, timedelta
+
+
+class SessionDBAuth(SessionExpAuth):
+    """ Session With expiary date """
+
+
+    def create_session(self, user_id=None):
+        """ Overiding init """
+    def user_id_for_session_id(self, session_id=None):
+        """ Overriding user_id_for_session_id"""
+        ...
+    def destroy_session(self, request=None):
+        """ Overridingdestroy_session """
+        ...
