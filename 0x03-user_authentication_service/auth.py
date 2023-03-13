c5235f87fa3495371516dad0c8f0de62d6316c50
Reid-T-W <rediettadesse100@gmail.com>
2023-03-13 23:04:31 +0300

    type annotation added for auth class and task 5 included in main

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,7 +20,7 @@ class Auth:
         """ Initializer for Auth class """
         self._db = DB()
 
-    def register_user(self, email, password):
+    def register_user(self, email: str, password: str):
         """ Register a user """
         try:
             user = self._db.find_user_by(email=email)
