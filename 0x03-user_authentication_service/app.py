79406580ff67ce21a85b738b05fdbed7ddce90a0
Reid-T-W <rediettadesse100@gmail.com>
2023-03-13 23:33:01 +0300

    user registration route complete

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 """ Route module for app """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def get_json():
+def get_json() -> str:
     """ Gets a static json message """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """ Registers a user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
