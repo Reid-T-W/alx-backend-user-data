d5b9dff8ed07ed51a787b78409a2e3b68d58a546
Reid-T-W <rediettadesse100@gmail.com>
2023-03-14 22:38:35 +0300

    profile route created

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,5 +57,15 @@ def logout() -> None:
     abort(403)
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """ Gets a users profile """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": f"{user.email}"}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
