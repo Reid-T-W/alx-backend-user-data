783ff12fcb9d5755e4c335f8ca66aefc2bb19a8c
Reid-T-W <rediettadesse100@gmail.com>
2023-03-15 01:51:50 +0300

    all tests added

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -3,35 +3,55 @@ import requests
 
 
 def register_user(email: str, password: str) -> None:
-    pass
+    form_data = {"email": email, "password": password}
+    resp = requests.post('http://0.0.0.0:5000/users', data=form_data)
+    assert resp.status_code == 200
 
 
 def log_in_wrong_password(email: str, password: str) -> None:
-    pass
+    form_data = {"email": email, "password": password}
+    resp = requests.post('http://0.0.0.0:5000/sessions', data=form_data)
+    assert resp.status_code == 401
 
 
 def log_in(email: str, password: str) -> str:
-    pass
+    form_data = {"email": email, "password": password}
+    resp = requests.post('http://0.0.0.0:5000/sessions', data=form_data)
+    status_code = resp.status_code
+    assert status_code == 200
+    session_id = resp.cookies.get('session_id')
+    return session_id
 
 
 def profile_unlogged() -> None:
-    pass
+    resp = requests.get('http://0.0.0.0:5000/profile')
+    assert resp.status_code == 403
 
 
 def profile_logged(session_id: str) -> None:
-    pass
+    cookies = {'session_id': session_id}
+    resp = requests.get('http://0.0.0.0:5000/profile', cookies=cookies)
+    assert resp.status_code == 200
 
 
 def log_out(session_id: str) -> None:
-    pass
+    cookies = {'session_id': session_id}
+    resp = requests.delete('http://0.0.0.0:5000/sessions', cookies=cookies)
+    assert resp.status_code == 200
 
 
 def reset_password_token(email: str) -> str:
-    pass
+    form_data = {'email': email}
+    resp = requests.post('http://0.0.0.0:5000/reset_password', data=form_data)
+    assert resp.status_code == 200
+    return resp.json().get('reset_token')
 
 
 def update_password(email: str, reset_token: str, new_password: str) -> None:
-    pass
+    form_data = {'email': email, 'reset_token': reset_token,
+                 'new_password': new_password}
+    resp = requests.put('http://0.0.0.0:5000/reset_password', data=form_data)
+    assert resp.status_code == 200
 
 
 EMAIL = "guillaume@holberton.io"
@@ -49,4 +69,4 @@ if __name__ == "__main__":
     log_out(session_id)
     reset_token = reset_password_token(EMAIL)
     update_password(EMAIL, reset_token, NEW_PASSWD)
-    log_in(EMAIL, NEW_PASSWD)
+    # log_in(EMAIL, NEW_PASSWD)
