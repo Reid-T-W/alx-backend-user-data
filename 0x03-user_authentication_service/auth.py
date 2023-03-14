8e550f41048bfd9605895cccb280de1ba6f09e8b
Reid-T-W <rediettadesse100@gmail.com>
2023-03-14 11:02:20 +0300

    user validation added

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,3 +28,16 @@ class Auth:
         except NoResultFound:
             hashed_pw = _hash_password(password)
             self._db.add_user(email, hashed_pw)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validates the user trying to login """
+        # Check if the user exists and retrieve the password
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        # Check if the password is correct
+        encoded_pw = password.encode('utf-8')
+        if bcrypt.checkpw(encoded_pw, user.hashed_password):
+            return True
+        return False
